0c602517175bfeb601bf72c04a35d66d7b37c4ca
Bruno Cardamone <brunocardamone03@gmail.com>
2024-12-30 18:37:44 -0300

    probamos modificaciones en la url

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost/merndb");
+    await mongoose.connect("mongodb://127.0.0.1:27017/merndb");
     console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
