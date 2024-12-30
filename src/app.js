aa8a4fd4f9bb274f392dcc3182df3225b431b2d5
Bruno Cardamone <brunocardamone03@gmail.com>
2024-12-30 17:59:36 -0300

    habilitamos url

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
-import express from "express"
-import morgan from "morgan"
-import authRoutes from "./routes/auth.routes.js"
+import express from "express";
+import morgan from "morgan";
+import authRoutes from "./routes/auth.routes.js";
 import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://test-three-chi-98.vercel.app", // Especifica el origen que puede hacer peticiones a tu API
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
+    allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos
+  })
+);
 
 app.use(express.json());
-app.use(morgan('dev'))
-app.use("/api", authRoutes)
+app.use(morgan("dev"));
+app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,4 +23,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-export default app
+export default app;
